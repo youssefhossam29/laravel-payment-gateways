28f477e7a170bd448549db17607e7608ed858925
youssefhossam29 <yh5719731@gmail.com>
2026-04-26 18:21:25 +0300

    feat: implement PaymentService for handling payment methods and gateways

---
+++ app/Services/Payments/PaymentService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services\Payments;
+
+use App\Interfaces\PaymentGatewayInterface;
+use App\Services\Payments\CODPaymentService;
+use Exception;
+
+class PaymentService
+{
+    public function mapMethodToGateway(string $method): string
+    {
+        return match ($method) {
+            'cod' => 'cod',
+            default => throw new Exception("Unsupported payment method"),
+        };
+    }
+
+    public function resolve(string $gateway): PaymentGatewayInterface
+    {
+        return match ($gateway) {
+            'cod' => app(CODPaymentService::class),
+            default => throw new Exception("Unsupported gateway"),
+        };
+    }
+
+    public function pay(array $data): string
+    {
+        $gateway = $this->mapMethodToGateway($data['payment_method']);
+        return $this->resolve($gateway)->pay($data);
+    }
+
+    public function handleCallback(string $gateway, $payload, $signature): bool
+    {
+        return $this->resolve($gateway)->handleCallback($payload, $signature);
+    }
+
+    public function handleResponse(array $payload, array $params): bool
+    {
+        $gateway = $params['gateway_type'] ?? null;
+        return $this->resolve($gateway)->handleResponse($payload, $params);
+    }
+}
