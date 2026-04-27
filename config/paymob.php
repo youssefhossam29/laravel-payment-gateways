1085363de6799ef2d15f94e24e70f043e780dc5d
youssefhossam29 <yh5719731@gmail.com>
2026-04-27 17:41:05 +0300

    feat: add Paymob configuration file for payment gateway integration

---
+++ config/paymob.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'api_key'      => env('PAYMOB_API_KEY'),
+    'public_key'   => env('PAYMOB_PUBLIC_KEY'),
+    'secret_key'   => env('PAYMOB_SECRET_KEY'),
+    'hmac_secret'  => env('PAYMOB_HMAC_SECRET'),
+    'base_url'     => env('PAYMOB_BASE_URL', 'https://accept.paymob.com/api'),
+
+    'integration' => [
+        'card'   => env('PAYMOB_CARD_INTEGRATION_ID'),
+        'wallet' => env('PAYMOB_WALLET_INTEGRATION_ID'),
+    ],
+
+    'frame_id' => [
+        'card'   => env('PAYMOB_CARD_FRAME_ID'),
+        'wallet' => env('PAYMOB_WALLET_FRAME_ID'),
+    ],
+];
