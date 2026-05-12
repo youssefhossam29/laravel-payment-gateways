d9a7846292043417e7851a8de1af1169a44c12e9
youssefhossam29 <yh5719731@gmail.com>
2026-05-12 19:12:16 +0300

    feat: add Stripe payment method support in PaymentService

--- app/Services/Payments/PaymentService.php
+++ app/Services/Payments/PaymentService.php
@@ -5,6 +5,7 @@ namespace App\Services\Payments;
 use App\Interfaces\PaymentGatewayInterface;
 use App\Services\Payments\PaymobPaymentService;
 use App\Services\Payments\CODPaymentService;
+use App\Services\Payments\StripePaymentService;
 use Exception;
 
 class PaymentService
@@ -13,6 +14,7 @@ class PaymentService
     {
         return match ($method) {
             'card', 'wallet' => 'paymob',
+            'stripe' => 'stripe',
             'cod' => 'cod',
             default => throw new Exception("Unsupported payment method"),
         };
@@ -22,6 +24,7 @@ class PaymentService
     {
         return match ($gateway) {
             'paymob' => app(PaymobPaymentService::class),
+            'stripe' => app(StripePaymentService::class),
             'cod' => app(CODPaymentService::class),
             default => throw new Exception("Unsupported gateway"),
         };
